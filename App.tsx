bdc4d4e2ba42b726500076401eb7c0e902e9770b
Daniel Espitia <daniel.espitiac@perficient.com>
2022-09-01 16:02:40 -0500

    added searchbar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Packages
+import { useState } from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+import { Searchbar } from 'react-native-paper';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onChangeSearch = (query: string) => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  search: {},
+  list: {
+    flex: 1,
+    padding: 16,
   },
 });
